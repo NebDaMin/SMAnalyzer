ac4c5eeff43a74cd550064bfe34f282161400349
beninja2 <benciummo@gmail.com>
2017-09-04 16:25:19 -0500

    Creating NetBeans Project
    C

---
+++ HumanDataAnalysisProject/src/humandataanalysisproject/HumanDataAnalysisProject.java
@@ -0,0 +1,16 @@
+package humandataanalysisproject;
+
+/**
+ *
+ * @author Ben Ciummo
+ */
+public class HumanDataAnalysisProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
