2aa3083b0ef17329bd817473f561b442579c5609
Ben Ciummo <benciummo@gmail.com>
2017-09-12 23:14:10 -0500

    Added dictionary file to reference for spell check. Still trying to get the check to a workable state.

--- HumanDataAnalysisProject/src/humandataanalysisproject/HumanDataAnalysisProject.java
+++ HumanDataAnalysisProject/src/humandataanalysisproject/HumanDataAnalysisProject.java
@@ -1,16 +1,31 @@
 package humandataanalysisproject;
 
+import com.swabunga.spell.event.*;
+import com.swabunga.spell.engine.*;
+import java.io.File;
+import java.io.IOException;
+
 /**
  *
  * @author Ben Ciummo
  */
-public class HumanDataAnalysisProject {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class HumanDataAnalysisProject 
+{
+    public static void main(String[] args) throws IOException 
+    {
+        File thefile = new File("externalfiles/BigAssDictionaryFromPrinceton.txt");
+        GenericSpellDictionary dictionary = new GenericSpellDictionary(thefile);
+        
+        
+        String teststring = new String("Hello world crap");
+        StringWordTokenizer finder = new StringWordTokenizer(teststring);
+        SpellChecker checker = new SpellChecker(dictionary);
+        int results = checker.checkSpelling(finder);
+        System.out.print(results);
+        checker.getSuggestions("thestring", 1);
+        
+               
+        
     }
     
 }
