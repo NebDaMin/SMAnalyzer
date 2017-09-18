664d5e11c7c2effc0a2ca3d0c6cced1c32ee4478
Ben Ciummo <benciummo@gmail.com>
2017-09-17 21:15:28 -0500

    BI also implemeted functionality to be able to be able to identify english comments. currently the word count part still counts everything, but that will be easily fixed. B D. I moved Matteo's changes into the src directory, I also added functionality to add all of the words to a hashmap,

--- HumanDataAnalysisProject/src/humandataanalysisproject/HumanDataAnalysisProject.java
+++ HumanDataAnalysisProject/src/humandataanalysisproject/HumanDataAnalysisProject.java
@@ -4,6 +4,11 @@ import com.swabunga.spell.event.*;
 import com.swabunga.spell.engine.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+//import java.util.Scanner;
 
 /**
  *
@@ -11,24 +16,71 @@ import java.io.IOException;
  */
 public class HumanDataAnalysisProject 
 {
+    private static ArrayList<CommentInstance> _AllComments;
+    
     public static void main(String[] args) throws IOException 
     {
-        //File thefile = new File("externalfiles/BigAssDictionaryFromPrinceton.txt");
-        //GenericSpellDictionary dictionary = new GenericSpellDictionary(thefile);
+        //Test strings to simulate reading in multiple comments.
+        String teststring =     "This is Bob. Bob is an english comment be like bob";
+        String teststring1 =    "THIZZZ hereeeeeee comment izzz nottttt speeeeellled cerecterly IZZZ NOOOOOT bob";
+        String teststring2 =    "This is a comment that doesn't necessarily agree with bob's ideaology however it still is english";
+        String teststring3 =    "The cake is a lie";
+        String teststring4 =    "This is a cooooment that has only a few missssspelled words therefore it should still be counted";
         
         
-        String teststring = new String("Hello world crap, now with punctuation and shit!");
+        //Adding all comments to an ArrayList
+        _AllComments = new ArrayList();
+        _AllComments.add(new CommentInstance(teststring));
+        _AllComments.add(new CommentInstance(teststring1));
+        _AllComments.add(new CommentInstance(teststring2));
+        _AllComments.add(new CommentInstance(teststring3));
+        _AllComments.add(new CommentInstance(teststring4));
         
-        CommentInstance testCI = new CommentInstance(teststring);
         
-        //StringWordTokenizer finder = new StringWordTokenizer(teststring);
+        //this loop gets the total number of unique words for every comment
+        //this number will be used to build a very large primative string array with every single word
+        int numOfWordsForAllComments = 0;
+        for(int z =0 ;z<_AllComments.size();z++)
+        {
+            numOfWordsForAllComments += _AllComments.get(z).getCommentNoPunctStringArray().length;
+        }
+        numOfWordsForAllComments--;
         
-        //SpellChecker checker = new SpellChecker(dictionary);
-        //int results = checker.checkSpelling(finder);
-        //System.out.print(results);
-        //checker.getSuggestions("thestring", 1);
         
-               
+        //This will concatinate all the arrays together into a single array
+        String[] allComments = new String[numOfWordsForAllComments+1];
+        int currentArrayPoint = 0;
+        for(int z =0;z<_AllComments.size();z++)
+        {
+            CommentInstance currentInstance = _AllComments.get(z);
+            System.arraycopy(   currentInstance.getCommentNoPunctStringArray(), 
+                                0,
+                                allComments, 
+                                currentArrayPoint,
+                                currentInstance.getCommentNoPunctStringArray().length);
+            currentArrayPoint += currentInstance.getCommentNoPunctStringArray().length;
+        }
         
+        
+        //Become one with the all powerful HashMap
+        HashMap rankedWords = compileAllCommentInstances(allComments);
+        System.out.println(rankedWords.toString());
+        
+        //Alice test
+        //String teststring = new Scanner(new File("externalfiles/alice.txt")).useDelimiter("\\Z").next();
+        //CommentInstance testCI = new CommentInstance(teststring);
+    }
+    
+    public static HashMap compileAllCommentInstances(String[] inputArray )
+    {
+        HashMap<String, Integer> frequencyHashMap = new HashMap<String, Integer>();
+        
+        for (String s : inputArray)
+        {
+            if (frequencyHashMap.containsKey(s)) frequencyHashMap.replace(s, frequencyHashMap.get(s) + 1);
+            else frequencyHashMap.put(s, 1);
+        }
+        
+        return frequencyHashMap;
     }
 }
