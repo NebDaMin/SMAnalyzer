9a293f61795cfe17c4514f6169d9b3c801630e0e
Whodicea <sparktrench@yahoo.com>
2017-10-15 20:03:11 -0500

    Added more error checking
    I've got more work to do on it, but I wanna see what the group has to say first

--- SMAnalyzer/src/main/Main_UI.java
+++ SMAnalyzer/src/main/Main_UI.java
@@ -1,9 +1,16 @@
 package main;
 
 /**
- * TODO: Add a checkbox for ignoring blacklist Add a checkbox for outputting to
- * .txt make an options jpanel output stuff to the ui add a silly easter egg
- * figure out why everything is errors Rename some stuff
+ * TODO: 
+ * add a silly easter egg
+ * decide if I want nicer code or more readable error checking 
+ * decide if I want JOptionPanes still
+ * bring the above two things to the group because I am indecisive
+ * fix a computer
+ * buy a graphics card
+ * replace a tower from the 90's
+ * sigh heavily at savings
+ * listen to Wintergatan and feel better
  */
 import java.awt.event.*;
 import javax.swing.*;
@@ -19,8 +26,8 @@ import javax.swing.JFileChooser;
 import main.fbinterface.FBClient;
 import main.humandataanalysisproject.*;
 
-public class Main_UI extends JFrame {
-
+public class Main_UI extends JFrame 
+{
     //UI vars
     // private JPanel mainPanel;
     private JPanel urlPanel;
@@ -44,7 +51,8 @@ public class Main_UI extends JFrame {
     private JLabel urlLabel;
     private JTextField urlText;
 
-    public Main_UI() throws IOException {
+    public Main_UI() throws IOException 
+    {
         //init
         ActionHandler ah = new ActionHandler();
         urlLabel = new JLabel("Url: ");
@@ -95,78 +103,114 @@ public class Main_UI extends JFrame {
 
     }
 
-    private class ActionHandler implements ActionListener {
+    private class ActionHandler implements ActionListener 
+    {
 
-        public void actionPerformed(ActionEvent e) {
-            if (e.getSource() == pasteButton) {
+        public void actionPerformed(ActionEvent e) 
+        {
+            if (e.getSource() == pasteButton) 
+            {
                 Clipboard clipboard = getToolkit().getSystemClipboard();
 
                 Transferable clipData = clipboard.getContents(this);
                 String s;
-                try {
+                try 
+                {
                     s = (String) (clipData.getTransferData(DataFlavor.stringFlavor));
-                } catch (Exception ex) {
+                } 
+                catch (Exception ex) 
+                {
                     s = ex.toString();
                 }
                 urlText.setText(s);
-            } else if (e.getSource() == analyzeButton) {
+            } 
+            else if (e.getSource() == analyzeButton) 
+            {
                 Analyzer.clearArray();
                 FBClient.clearArray();
                 String urlString = urlText.getText();
 
-                if (urlString.equals(null) || urlString.equals("")) {
+                if (urlString.equals(null) || urlString.equals("")) 
+                {
                     JOptionPane.showMessageDialog(null, "There's nothing to analyze.\nPlease paste a url.", "Error", JOptionPane.ERROR_MESSAGE);
-                } else {
+                } 
+                else 
+                {
                     HashMap<String, String> stringMap = parseUrl(urlString);
-                    try {
-                        Boolean child = childCommentBox.isSelected();
-                        Boolean blacklist = blacklistIgnoreBox.isSelected();
-                        Boolean file = fileBox.isSelected();
+                    Boolean child = childCommentBox.isSelected();
+                    Boolean blacklist = blacklistIgnoreBox.isSelected();
+                    Boolean file = fileBox.isSelected();
 
-                        if (file) {
-                            JFileChooser chooser = new JFileChooser();
-                            chooser.setCurrentDirectory(new File("."));
-                            int result = chooser.showSaveDialog(Main_UI.this);
-                            if (result == JFileChooser.APPROVE_OPTION) {
-                                File f = chooser.getSelectedFile();
-                                if (f.exists() == true) {
-                                    int n = JOptionPane.showConfirmDialog(Main_UI.this,
-                                            "This file already exists. Would you like to overwrite this file?",
-                                            "Confirmation", JOptionPane.YES_NO_OPTION);
-                                    if (n == JOptionPane.YES_OPTION) {
-                                        try {
-                                            out = new PrintWriter(new FileOutputStream(f, false));
-                                            out.close();
-                                        } catch (IOException ex) {
-                                            JOptionPane.showMessageDialog(Main_UI.this, "File could not be opened.",
-                                                    "Get a better file", JOptionPane.INFORMATION_MESSAGE);
+                    if (file) 
+                    {
+                        JFileChooser chooser = new JFileChooser();
+                        chooser.setCurrentDirectory(new File("."));
+                        int result = chooser.showSaveDialog(Main_UI.this);
+                        if (result == JFileChooser.APPROVE_OPTION) 
+                        {
+                            File f = chooser.getSelectedFile();
+                            if (f.exists() == true) 
+                            {
+                                int n = JOptionPane.showConfirmDialog(Main_UI.this,
+                                    "This file already exists. Would you like to overwrite this file?",
+                                    "Confirmation", JOptionPane.YES_NO_OPTION);
+                                if (n == JOptionPane.YES_OPTION) 
+                                {
+                                    try 
+                                    {
+                                        out = new PrintWriter(new FileOutputStream(f, false));
+                                        out.close();
+                                    } catch (IOException ex) 
+                                    {
+                                        JOptionPane.showMessageDialog(Main_UI.this, "File could not be opened.",
+                                                "Get a better file", JOptionPane.INFORMATION_MESSAGE);
                                         }
                                     }
                                 }
                             }
                         }
-                        if (stringMap.size() == 1) {
+                        if (stringMap.size() == 1) 
+                        {
                             FBClient.fetchRandomPagePost(stringMap.get("Page Name"), child);
-                        } else if (stringMap.size() == 3) {
+                        } 
+                        else if (stringMap.size() == 3) 
+                        {
                             FBClient.fetchSpecificPagePost(stringMap.get("Page Name"), stringMap.get("Post Id"), child);
                         }
-
-                        Analyzer.setComments(FBClient.getPostArray());
-                        //get group output data, format and set in outputText
-                        ArrayList<CommentGroup> groups = Analyzer.groupComments();
-                        String outputString = "Total Groups: " + groups.size();
-                        outputString += "\n----------------------------------\n";
-                        for (CommentGroup g : groups) {
-                            outputString += g;
+                        try 
+                        {         
+                            Analyzer.setComments(FBClient.getPostArray());
+                            //get group output data, format and set in outputText
+                            ArrayList<CommentGroup> groups = Analyzer.groupComments();
+                            String outputString = "Total Groups: " + groups.size();
+                            outputString += "\n----------------------------------\n";
+                            for (CommentGroup g : groups) 
+                            {
+                                outputString += g;
+                            }
+                            outputText.setText(outputString);
+                            outputPanel.setVisible(true);
+                        } 
+                        catch (IndexOutOfBoundsException ioobe)
+                        {
+                            //this one I've seen but shouldn't ever happen if the code is working
+                            System.out.println(ioobe);
                         }
-                        outputText.setText(outputString);
-                        outputPanel.setVisible(true);
-                    } catch (NullPointerException npe) {
-                        System.out.println(npe);
-                    } catch (Exception ex) {
-                        System.out.println(ex);
-                        JOptionPane.showMessageDialog(null, "Something Broke", "Error", JOptionPane.ERROR_MESSAGE);
-                    }
+                        catch (IOException ioe)
+                        {
+                            //this thing has to be thrown for the analyzer code
+                            System.out.println(ioe);
+                        }
+                        catch (NullPointerException npe)
+                        {
+                            //the facebook stuff usually gives this error if bad happens
+                             System.out.println(npe);
+                        } 
+                        catch (Exception ex) 
+                        {
+                            System.out.println(ex);
+                            JOptionPane.showMessageDialog(null, "Something Broke", "Error", JOptionPane.ERROR_MESSAGE);
+                        }
                 }
             } else {
                 JOptionPane.showMessageDialog(Main_UI.this, "Uh....",
@@ -177,8 +221,8 @@ public class Main_UI extends JFrame {
 
     public HashMap<String, String> parseUrl(String s) {
         if (!s.contains("facebook.com")) {
-            JOptionPane.showMessageDialog(Main_UI.this, "Uh....",
-                    "Url not recognized", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(Main_UI.this, "Url not recognized",
+                    "We only do facebook", JOptionPane.INFORMATION_MESSAGE);
             return null;
         }
 
@@ -195,13 +239,13 @@ public class Main_UI extends JFrame {
             map.put("Post Type", array[1]);
             map.put("Post Id", array[2]);
             if (array[2].equals("photos")) {
-                JOptionPane.showMessageDialog(Main_UI.this, "Uh....",
-                        "We cannot parse photo posts yet", JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(Main_UI.this, "We cannot parse photo posts yet",
+                        "Wrong.", JOptionPane.INFORMATION_MESSAGE);
                 return null;
             }
         } else {
-            JOptionPane.showMessageDialog(Main_UI.this, "Uh....",
-                    "Url not recognized", JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(Main_UI.this, "Url not recognized",
+                    "Uh...", JOptionPane.INFORMATION_MESSAGE);
             return null;
         }
 
