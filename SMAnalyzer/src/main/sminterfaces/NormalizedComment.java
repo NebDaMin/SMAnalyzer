768608f881e8480982b0e52ee756541459be00ab
kailee <kmcalhoun42@gmail.com>
2017-10-30 22:42:12 -0500

    normalize

--- SMAnalyzer/src/main/sminterfaces/NormalizedComment.java
+++ SMAnalyzer/src/main/sminterfaces/NormalizedComment.java
@@ -1,6 +1,49 @@
 package main.sminterfaces;
 
+import org.json.*;
 
 public class NormalizedComment {
 
+    String media;
+    String id;
+    String message;
+    String posted_at;
+    String shares;
+
+    public void setFromFacebook(JSONObject fb) {
+        this.media = "facebook";
+        try {
+            this.id = fb.getString("id");
+            this.message = fb.getString("message");
+            this.posted_at = fb.getString("created_time");
+            this.shares = fb.getJSONObject("shares").getString("count");
+        } catch (JSONException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void setFromYoutube(JSONObject yt) {
+        this.media = "youtube";
+        try {
+            
+        } catch (JSONException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void setFromTwitter(JSONObject twt) {
+        this.media = "twitter";
+        try {
+        } catch (JSONException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void setFromReddit(JSONObject rdt) {
+        this.media = "reddit";
+        try {
+        } catch (JSONException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
