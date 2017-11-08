19ce8742b046f40add113f099ac5774250ba91dc
kmcalhoun42@gmail.com <kmcalhoun42@gmail.com>
2017-11-07 22:20:54 -0600

    FBClient was hurt.. It's better now though.

--- SMAnalyzer/src/main/sminterfaces/FBClient.java
+++ SMAnalyzer/src/main/sminterfaces/FBClient.java
@@ -1,36 +1,20 @@
 package main.sminterfaces;
 
-<<<<<<< HEAD:SMAnalyzer/src/main/sminterfaces/FBClient.java
 import smsdk.*;
-=======
-import fbapi.*;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
->>>>>>> master:SMAnalyzer/src/main/fbinterface/FBClient.java
 import java.time.LocalDateTime;
 import java.util.*;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
 import org.json.*;
-        
+
 public class FBClient {
 
     public static final String APP_ID = "1959837484256375";
     public static final String APP_SECRET = "b224ad6a4bae050c34fff51efbce2b60";
     static FbAPI fbClient;
-<<<<<<< HEAD:SMAnalyzer/src/main/sminterfaces/FBClient.java
     private ArrayList<NormalizedComment> PostArrayList;
 
-=======
-    private ArrayList<JSONObject> PostArrayList;
-    private PrintWriter out;
-    
->>>>>>> master:SMAnalyzer/src/main/fbinterface/FBClient.java
     public FBClient() {
         fbClient = new FbAPI(APP_ID, APP_SECRET);
         PostArrayList = new ArrayList();
@@ -45,60 +29,10 @@ public class FBClient {
             params.put("limit", 1);
             JSONObject pageFeed = fbClient.getConnections(pageName, "feed", params);
             List<JSONObject> postList = fbClient.convertJsonDataToList(pageFeed);
-<<<<<<< HEAD:SMAnalyzer/src/main/sminterfaces/FBClient.java
             for (JSONObject post : postList) {
                 NormalizedComment normComment = new NormalizedComment();
                 normComment.setFromFacebook(post);
                 PostArrayList.add(normComment);
-=======
-            if (file) 
-            {
-                JFileChooser chooser = new JFileChooser();
-                chooser.setCurrentDirectory(new File("."));
-                int result = chooser.showSaveDialog(null);
-                if (result == JFileChooser.APPROVE_OPTION) {
-                    File f = chooser.getSelectedFile();
-                    if (f.exists() == true) {
-                        int n = JOptionPane.showConfirmDialog(null,
-                         "This file already exists. Would you like to overwrite this file?",
-                        "Confirmation", JOptionPane.YES_NO_OPTION);
-                        if (n == JOptionPane.YES_OPTION) {
-                            try {
-                            out = new PrintWriter(new BufferedWriter(new FileWriter(f, false)));
-                            } catch (IOException ex) {
-                             JOptionPane.showMessageDialog(null, "File could not be opened.",
-                             "Get a better file", JOptionPane.INFORMATION_MESSAGE);
-                            }
-                        }
-                    }
-                    else{
-                        try {
-                            out = new PrintWriter(new BufferedWriter(new FileWriter(f)));
-                        } 
-                        catch(IOException ex)
-                        {
-                        JOptionPane.showMessageDialog(null, "File could not be opened.",
-                        "Get a better file", JOptionPane.INFORMATION_MESSAGE);
-                        }
-                    }
-                } 
-            }    
-             if(file){
-                for (JSONObject post : postList) {
-                PostArrayList.add(post);
-                out.println("Post id: " + post.getString("id"));
-                out.println("Message: " + post.getString("message"));
-                out.println("Created on: " + post.getString("created_time"));
-                out.println(); 
-                
-                getComments(post.getString("id"), children);
-                }
-            out.close();
-            }
-             else{
-             for (JSONObject post : postList) {
-                PostArrayList.add(post);
->>>>>>> master:SMAnalyzer/src/main/fbinterface/FBClient.java
                 System.out.println("Post id: " + post.getString("id"));
                 System.out.println("Message: " + post.getString("message"));
                 System.out.println("Created on: " + post.getString("created_time"));
@@ -106,9 +40,8 @@ public class FBClient {
 
                 getComments(post.getString("id"), children);
             }
-             }
-            System.out.println("PostArrayList size: " + PostArrayList.size());
         }
+        System.out.println("PostArrayList size: " + PostArrayList.size());
     }
 
     public void fetchSpecificPagePost(String pageName, String postId, Boolean children, Boolean file) {
